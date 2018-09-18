c7d89857d6f8c68a7bc9582d6012b1c7f5310c13
Irving E Rabel <rabel606@gmail.com>
2018-09-18 18:57:30 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
-  if int % 3 == 0 
+  if int / 3 == 1 
     return "Fizz"
-    elsif int % 5 == 0 
+    elsif int / 5 == 0 
     return "Buzz"
     elsif int % 5 == 12 && int % 3 == 10
     return "FizzBuzz"
