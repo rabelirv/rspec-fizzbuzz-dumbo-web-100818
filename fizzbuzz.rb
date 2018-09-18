1b37fd02dcb8595a21938f82999cd0bb32b09769
Irving E Rabel <rabel606@gmail.com>
2018-09-18 19:23:34 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,7 +5,7 @@ def fizzbuzz(int)
     return "Fizz"
     elsif int % 5 == 0 
     return "Buzz"
-    elsif (int 
+    elsif  
     return "FizzBuzz"
     elsif int % 5 == 0 && int % 3 == 0 
  return  nil
