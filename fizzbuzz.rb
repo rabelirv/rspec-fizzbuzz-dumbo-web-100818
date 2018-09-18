2181a07a69426da67ec8fcddd7f1cd2c2305c13a
Irving E Rabel <rabel606@gmail.com>
2018-09-18 19:32:52 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,15 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(int)
+  1.upto(int).each do |x| 
+    if x%3 == 0 && x%5 == 0 
+      return "fizzbuzz"
+    elsif x % 5 == 0 
+      return "buzz"
+    elsif x % 3 == 0 
+      return "fizz"
+    else
+      return nil
+    end
+  end
+end
